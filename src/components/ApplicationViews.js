1810d5445464d62433f2d7f5397cf2698431851a
Steve Brownlee <chortlehoort@gmail.com>
2018-07-16 22:45:34 -0500

    Using fetch to populate state from json-server API

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,6 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
+import settings from "../settings"
 import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
@@ -7,45 +8,31 @@ import EmployeeList from './employee/EmployeeList'
 
 export default class ApplicationViews extends Component {
     state = {
-        employees: [
-            { id: 1, name: "Jessica Younker" },
-            { id: 2, name: "Jordan Nelson" },
-            { id: 3, name: "Zoe LeBlanc" },
-            { id: 4, name: "Blaise Roberts" }
-        ],
-        locations: [
-            { id: 1, name: "Nashville North" },
-            { id: 2, name: "Nashville South" }
-        ],
-        animals: [
-            { id: 1, name: "Doodles" },
-            { id: 2, name: "Jack" },
-            { id: 3, name: "Angus" },
-            { id: 4, name: "Henley" },
-            { id: 5, name: "Derkins" },
-            { id: 6, name: "Checkers" }
-        ],
-        owners: [
-            { id: 1, name: "Ryan Tanay" },
-            { id: 2, name: "Emma Beaton" },
-            { id: 3, name: "Dani Adkins" },
-            { id: 4, name: "Adam Oswalt" },
-            { id: 5, name: "Fletcher Bangs" },
-            { id: 6, name: "Angela Lee" }
-        ]
+        data: {
+            locations: [],
+            animals: [],
+            employees: []
+        }
     }
+
+    componentDidMount() {
+        fetch(`${settings.remoteURL}/db`)
+            .then(e => e.json())
+            .then(data => this.setState({ data: data }))
+    }
+
 
     render() {
         return (
             <React.Fragment>
                 <Route exact path="/" render={(props) => {
-                    return <LocationList locations={this.state.locations} />
+                    return <LocationList locations={this.state.data.locations} />
                 }} />
                 <Route exact path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                    return <AnimalList animals={this.state.data.animals} />
                 }} />
                 <Route exact path="/employees" render={(props) => {
-                    return <EmployeeList employees={this.state.employees} />
+                    return <EmployeeList employees={this.state.data.employees} />
                 }} />
             </React.Fragment>
         )
---
+++ src/settings.js
@@ -0,0 +1,4 @@
+export default {
+    // remoteURL: "https://nss-kitty-purry.herokuapp.com"
+    remoteURL: "http://localhost:5002"
+}
