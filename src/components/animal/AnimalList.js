39b2aaff21f9af7157a4844e5527a987031c414d
Steve Brownlee <chortlehoort@gmail.com>
2018-07-31 08:03:37 -0500

    Line breaks for legibility

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react"
 import "./Animal.css"
-import AnimalCard from "./AnimalCard";
+import AnimalCard from "./AnimalCard"
 
 export default class AnimalList extends Component {
     render () {
         return (
             <React.Fragment>
                 <div className="animalButton">
-                    <button type="button" onClick={()=>{this.props.history.push("/animals/new")}} className="btn btn-success">Admit Animal</button>
+                    <button type="button"
+                            className="btn btn-success"
+                            onClick={() => {
+                                this.props.history.push("/animals/new")}
+                            }>
+                        Admit Animal
+                    </button>
                 </div>
                 <section className="animals">
                 {
